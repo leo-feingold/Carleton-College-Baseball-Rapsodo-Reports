5219e95f3240e7cbae3cd10ed67cfb89bbc35110
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2025-02-28 16:25:28 -0600

    rolling FB Data

--- Rapsodo Reports/test.py
+++ Rapsodo Reports/test.py
@@ -16,11 +16,33 @@ def load_data(file_path):
 
     return df
 
+def date(df):
+    print(df.Date)
+
+def rollingFastballVelo(df):
+
+    df_fastball = df.loc[df["Pitch Type"] == "Fastball"]
+    df_fastball = df_fastball.sort_values(by="Date", ascending=True)
+    print(len(df_fastball))
+    df_fastball["Rolling Velo"] = df_fastball["Velocity"].rolling(window=10).mean()
+
+
+    plt.figure(figsize=(10,5))
+    plt.plot(df_fastball["Date"], df_fastball["Rolling Velo"], marker="o", linestyle="-", label="Rolling 10-Pitch Fastball Velo")
+    plt.xlabel("Date")
+    plt.ylabel("Velocity (mph)")
+    plt.title("Rolling 10-Pitch Average Velocity (Fastballs)")
+    plt.legend()
+    plt.grid(True)
+    num_ticks = 5  # Adjust the number of ticks shown
+    plt.xticks(df_fastball["Date"][::len(df_fastball)//num_ticks], rotation=20)
+    plt.show()
+
 
 def main():
-    df = load_data("/Users/leofeingold/Documents/GitHub/Carleton-College-Baseball-Rapsodo-Reports/Ananth_Data/940780_pitching_b3f83e223b9bb1197284.csv")
-    print(df.columns)
-
+    df = load_data("/Users/leofeingold/Documents/GitHub/Carleton-College-Baseball-Rapsodo-Reports/Ananth_Data/940780_pitching_8328afd9242cc19906c4.csv")
+    rollingFastballVelo(df)
+    #date(df)
 
 
 
